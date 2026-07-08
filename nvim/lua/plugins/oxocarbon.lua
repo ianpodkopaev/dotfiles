17112040adc1fd2f6905ac81d3ed8c248fcb766c
ianpodkopaev@myasnikova <ian.podkopaev@gmail.com>
2026-07-08 10:13:07 +0000

    upd nvim

---
+++ nvim/lua/plugins/oxocarbon.lua
@@ -0,0 +1,10 @@
+-- Wallust Theme Configuration (wallpaper-based)
+return {
+  -- Wallust generates this file when wallpaper changes
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "wallust",
+    },
+  },
+}
---
+++ nvim/lua/wallust-colors.lua
@@ -0,0 +1,24 @@
+-- /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  --
+-- wallust template - colors-nvim
+
+return {
+  foreground = "#141414",
+  background = "#FFFFFF",
+  cursor = "#6A6A6A",
+  color0 = "#FFFFFF",
+  color1 = "#AAAAAA",
+  color2 = "#BFC0C0",
+  color3 = "#9C9D9C",
+  color4 = "#8B8B8B",
+  color5 = "#585858",
+  color6 = "#2C2C2C",
+  color7 = "#141414",
+  color8 = "#888989",
+  color9 = "#AAAAAA",
+  color10 = "#BFC0C0",
+  color11 = "#9C9D9C",
+  color12 = "#8B8B8B",
+  color13 = "#585858",
+  color14 = "#2C2C2C",
+  color15 = "#070707",
+}
